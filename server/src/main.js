7e22b1b401b14749ada359f19731411302ed37b5
joulis1derful <joulis1derful@gmail.com>
2018-11-17 21:36:55 +0200

    added new routes

---
+++ server/src/main.js
@@ -0,0 +1,37 @@
+const processor = require('./business/processor')
+
+const express = require('express')
+const app = express()
+
+const APP_PORT = 3000
+
+app.use('/', (req, res, next) => {
+    console.log('y1')
+    next()
+})
+
+app.get('/movies/top', (req, res) => {
+    res.send('TOP movies')
+})
+
+app.get('/movies/recent', (req, res) => {
+    res.send('Recent')
+})
+
+app.get('/movies/search', async (req, res) => {
+    if (!req.query.name) {
+        res.sendStatus(403)
+    } else {
+        const movies = await processor.findMovieByName(req.query.name)
+        res.json(movies)
+    }
+})
+
+app.get('/movies/:id', (req, res) => {
+    // await processor.insertOn
+    res.send(req.params.id)
+})
+
+app.listen(APP_PORT, () => {
+    console.log('Server is listening on port ' + APP_PORT)
+})
