563c471c4dc4a81d0a959554f6c001b1d56149bb
joulis1derful <joulis1derful@gmail.com>
2018-11-17 21:37:22 +0200

    Added basic business logic

---
+++ server/src/business/processor.js
@@ -0,0 +1,35 @@
+const storage = require('../domain/movie')
+const axios = require('axios')
+const dotenv = require("dotenv").config()
+
+const API_KEY = process.env.API_KEY
+const SEARCH_MOVIES_URL = process.env.SEARCH_MOVIES_URL
+const SINGLE_MOVIE_URL = process.env.SINGLE_MOVIE_URL
+
+// TODO: insert to db if wasn't found at local db and was found on tmdb
+const findMovieByName = async (name) => {
+    const movies = await storage.findMovies(name)
+    if (movies.length > 0) {
+        return movies
+    }
+    return axios.get(`${SEARCH_MOVIES_URL}?api_key=${API_KEY}&query=${name}`)
+         .then((response) => { return response.data.results })
+         .catch((err) => { throw new Error(err) })
+}
+
+// TODO: insert to db if wasn't found at local db and was found on tmdb
+const findMovieById = async (id) => {
+    const movie = await storage.findMovie(id)
+    console.log(movie)
+    if (movie) {
+        return movie
+    }
+    return axios.get(`${SINGLE_MOVIE_URL}/${id}?api_key=${API_KEY}`)
+         .then((response) => { return response.data })
+         .catch((err) => { throw new Error(err) })
+}
+
+module.exports = {
+    findMovieById,
+    findMovieByName
+}
--- server/src/domain/movie.js
+++ server/src/domain/movie.js
@@ -1,21 +1,48 @@
 const dotenv = require("dotenv").config()
-const mongoClient = require("mongodb").MongoClient;
-const dbUrl = process.env.DB_URL;
-const dbName = process.env.DB_NAME;
+const mongoClient = require("mongodb").MongoClient
 
-const insertToDb = async (dbName, movie) => {
+const dbUrl = process.env.DB_URL
+const dbName = process.env.DB_NAME
+
+const insertMovie = async (movie) => {
     const client = getConnection(dbUrl)
     const db = client.db(dbName)
-    await db.collection('movie').insertOne(movie);
+    await db.collection('movie').insertOne(movie)
     
-    client.close();
+    client.close()
+}
+
+const findMovies = async (name) => {
+    const client = await getConnection(dbUrl)
+    const database = await client.db(dbName)
+    const movies = await database.collection('movie').find({ name: name }).toArray()
+    client.close()
+
+    return movies
+}
+
+const findMovie = async (id) => {
+    const client = getConnection(dbUrl)
+    const db = client.db(dbName)
+    const movie = await db.collection('movie').findOne({ movieId: id })
+    client.close()
+
+    return movie
 }
 
 const getConnection = (url) => {
-    mongoClient.connect(url, (err, client) => {
-        if (err) {
-            throw new Error(err);
-        }
-        return client
-    });
+    return new Promise((resolve, reject) => {
+        mongoClient.connect(url, (err, client) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(client)
+        })
+    }) 
+}
+
+module.exports = {
+    insertMovie,
+    findMovie,
+    findMovies
 }
