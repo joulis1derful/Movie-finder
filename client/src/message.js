e78c32ba5fbd4680e8bb5df11295b154929216ec
joulis1derful <joulis1derful@gmail.com>
2019-06-08 18:24:32 +0300

    get rid of redundant code

--- client/src/message.js
+++ client/src/message.js
@@ -3,8 +3,6 @@ import MessageComponent from './components/Message.vue'
 
 const MessageConstructor = Vue.extend(MessageComponent);
 
-let instance
-
 const Message = function(options) {
   options = options || {}
   if (typeof options === 'string') {
@@ -13,26 +11,13 @@ const Message = function(options) {
     }
   }
 
-  instance = new MessageConstructor({
+  const instance = new MessageConstructor({
     data: options
   })
   instance.$mount()
   document.body.appendChild(instance.$el)
  
-  instance.visible = true
   return instance
 };
-
-['success', 'warning', 'info', 'error'].forEach(type => {
-  Message[type] = options => {
-    if (typeof options === 'string') {
-      options = {
-        message: options
-      }
-    }
-    options.type = type
-    return Message(options)
-  }
-})
 
 export default Message;
